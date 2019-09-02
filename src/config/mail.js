ac31c88dfdaa606b7f52fa7932d5cdd08edeff8c
Paulo Edson <jovemp@hotmail.com>
2019-09-02 20:32:12 -0300

    Criado email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '868eb4ea923b30',
+    pass: '9803d936b7d2f2',
+  },
+  default: {
+    from: 'Paulo Edson <noreply@hotmail.com>',
+  },
+};
